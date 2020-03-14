4c61a9bf342e02e43454274221c7896db43c0d98
Francisco Rodrigues <frchico@users.noreply.github.com>
2020-03-13 21:34:13 -0300

    Atualizações no layout do site (#73)
    * Melhorias na listagem de publicações (#69)
    
    * Melhorando a tela de publicações
    
    * Melhorias no visual
    
    * Arredondando as bordas das publicações
    
    * Melhoria na paginação
    
    * Tentativa de solução para o plugin de paginação (#71)
    
    * Melhorias na página inicial do site (#70)
    
    * Melhorando a tela de publicações
    
    * Melhorias no visual
    
    * Arredondando as bordas das publicações
    
    * Melhoria na paginação
    
    * Melhorias na página inicial
    
    * Melhorias em: Faça parte do time
    
    * Melhorias na localização
    
    * logo dos parceiros
    
    * Melhorias solicitadas
    
    Co-authored-by: Francisco Rodrigues <frchico@users.noreply.github.com>
    
    * Atualizando o arquivo app.js (#72)
    
    * solução para o plugin de paginação
    
    * Correção de erro das imagens dos parceiros
    
    Co-authored-by: Reinan Gabriel <reinangabriel1520@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,42 @@ $().ready(function () {
   $('#myCarousel').carousel({
     interval: 4000
   });
+
+  function filterBlur(elem, valor) {
+    $(elem).css('-webkit-filter', 'blur(' + valor + 'px)');
+    $(elem).css('filter', 'blur(' + valor + 'px)');
+    $(elem).css('-moz-filter', 'blur(' + valor + 'px)');
+    $(elem).css('-o-filter', 'blur(' + valor + 'px)');
+    $(elem).css('-ms-filter', 'blur(' + valor + 'px)');
+  }
+
+  $('.post.post-thumb .post-title a').hover(function () {    
+    var result = $(this).parent().parent().parent().find('.post-img');
+    filterBlur(result[0], 2);
+  });
+  
+  $('.post.post-thumb .post-title a').mouseout(function(){
+    var result = $(this).parent().parent().parent().find('.post-img');
+    filterBlur(result[0], 0);
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  })
+  
 });
 
 function rotateCard(btn) {
